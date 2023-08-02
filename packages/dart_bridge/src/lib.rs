6043e7d6fc52aec9d01f4b8eef2d93c00d40d727
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-08-02 22:13:04 +0300

    refactor(rust): Declutter.

--- packages/dart_bridge/src/lib.rs
+++ packages/dart_bridge/src/lib.rs
@@ -1,23 +1,23 @@
-#![allow(non_camel_case_types)]
-
+use std::{
+    collections::HashMap,
+    ffi::c_void,
+    sync::Mutex,
+};
 use dart_sys::{
+    Dart_Handle,
     Dart_PersistentHandle,
     Dart_InitializeApiDL,
-    // Dart_NewPersistentHandle_DL,
-    // Dart_HandleFromPersistent_DL,
-    // Dart_DeletePersistentHandle_DL,
 };
-use std::{sync::Mutex, collections::HashMap, ffi::c_void};
 use once_cell::sync::Lazy;
 
-pub type Dart_Handle = dart_sys::Dart_Handle;
-pub type CallbackInvoker = fn(handle: Dart_Handle, msg: *const u8, msgSize: usize) -> ();
+pub type CallbackInvoker = fn(callbackHandle: Dart_Handle, msg: *const u8, msgSize: usize);
 
-static mut CALLBACK_INVOKER: Option<CallbackInvoker> = None;
-static mut CALLBACKS: Lazy<Mutex<HashMap<*const u8, Dart_PersistentHandle>>> = Lazy::new(|| {
+static mut HANDLES: Lazy<Mutex<HashMap<*const u8, Dart_PersistentHandle>>> = Lazy::new(|| {
     let m: HashMap<*const u8, Dart_PersistentHandle> = HashMap::new();
     Mutex::new(m)
 });
+
+static mut CB_INVOKER: Option<CallbackInvoker> = None;
 
 #[no_mangle]
 pub unsafe extern "C" fn init(data: *mut c_void) -> isize {
@@ -26,51 +26,50 @@ pub unsafe extern "C" fn init(data: *mut c_void) -> isize {
     }
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn register_invoker(invoker: *const CallbackInvoker) -> () {
-    CALLBACK_INVOKER = Some(*(invoker.clone()));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn register_callback(cb: Dart_Handle, id: *const u8) -> () {
-    match CALLBACKS.try_lock() {
-        Ok(mut lock) => {
-            lock.insert(id.clone(), cb);
-        },
-        Err(_) => return,
+unsafe fn add_callback(handle: Dart_PersistentHandle, key: *const u8) {
+    unsafe {
+        let handles = HANDLES.get_mut();
+        if let Ok(handles) = handles {
+            let entry = handles.entry(key).or_insert(handle);
+            *entry = handle;
+        }
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn invoke(msg: *const u8, msg_len: usize, id: *const u8) -> () {
-    match CALLBACKS.try_lock() {
-        Ok(lock) => {
-            match lock.get(&id.clone()) {
-                Some(cb) => {
-                    match CALLBACK_INVOKER {
-                        Some(invoker) => {
-                            invoker(*cb, msg.clone(), msg_len)
-                        },
-                        None => return,
-                    }
-                },
-                None => return,
-            }
-        },
-        Err(_) => return,
+pub unsafe extern "C" fn register_invoker(invoker: *const CallbackInvoker) {
+    unsafe {
+        CB_INVOKER = Some(*invoker);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn release() -> () {
-    match CALLBACKS.try_lock() {
-        Ok(lock) => {
-            for (_, _handle) in lock.iter() {
-                // unsafe {
-                //     Dart_DeletePersistentHandle_DL(handle)
-                // }
+pub unsafe extern "C" fn register_callback(handle: Dart_PersistentHandle, key: *const u8) {
+    unsafe {
+        add_callback(handle, key);
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn invoke(msg: *const u8, msg_len: usize, key: *const u8) {
+    unsafe {
+        let handles = HANDLES.get_mut();
+        if let Ok(handles) = handles {
+            if let Some(handle) = handles.get(&key) {
+                if let Some(invoker) = CB_INVOKER {
+                    invoker(*handle, msg, msg_len);
+                }
             }
-        },
-        Err(_) => return,
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn release() {
+    unsafe {
+        let handles = HANDLES.get_mut();
+        if let Ok(handles) = handles {
+            handles.clear();
+        }
     }
 }
