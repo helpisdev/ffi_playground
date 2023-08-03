e819e177b3fddada727f8c0037a4c6d72d48ff95
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-08-03 20:52:11 +0300

    feat(rust): Match lib.c API.

--- packages/dart_bridge/src/lib.rs
+++ packages/dart_bridge/src/lib.rs
@@ -1,23 +1,30 @@
 use std::{
     collections::HashMap,
-    ffi::c_void,
+    ffi::{
+        c_void,
+        CStr,
+        c_char,
+    },
     sync::Mutex,
 };
 use dart_sys::{
     Dart_Handle,
     Dart_PersistentHandle,
+    Dart_NewPersistentHandle_DL,
+    Dart_HandleFromPersistent_DL,
+    Dart_DeletePersistentHandle_DL,
     Dart_InitializeApiDL,
 };
 use once_cell::sync::Lazy;
 
-pub type CallbackInvoker = fn(callbackHandle: Dart_Handle, msg: *const u8, msgSize: usize);
+pub type CallbackInvoker = unsafe extern "C" fn(callbackHandle: Dart_Handle, msg: *const c_char, msgSize: usize);
 
-static mut HANDLES: Lazy<Mutex<HashMap<*const u8, Dart_PersistentHandle>>> = Lazy::new(|| {
-    let m: HashMap<*const u8, Dart_PersistentHandle> = HashMap::new();
+static mut HANDLES: Lazy<Mutex<HashMap<String, Dart_PersistentHandle>>> = Lazy::new(|| {
+    let m: HashMap<String, Dart_PersistentHandle> = HashMap::new();
     Mutex::new(m)
 });
 
-static mut CB_INVOKER: Option<CallbackInvoker> = None;
+static mut CB_INVOKER: Option<*const CallbackInvoker> = None;
 
 #[no_mangle]
 pub unsafe extern "C" fn init(data: *mut c_void) -> isize {
@@ -26,12 +33,18 @@ pub unsafe extern "C" fn init(data: *mut c_void) -> isize {
     }
 }
 
-unsafe fn add_callback(handle: Dart_PersistentHandle, key: *const u8) {
+unsafe fn add_callback(handle: Dart_Handle, key: *const c_char) {
     unsafe {
         let handles = HANDLES.get_mut();
         if let Ok(handles) = handles {
-            let entry = handles.entry(key).or_insert(handle);
-            *entry = handle;
+            let c_str: &CStr = CStr::from_ptr(key);
+            if let Ok(id) = c_str.to_str() {
+                if let Some(f) = Dart_NewPersistentHandle_DL {
+                    let handle = f(handle);
+                    handles.insert(String::from(id), handle);
+                    println!("Registering callback \"{}\" with handle: {:#?}", id, handle);
+                }
+            }
         }
     }
 }
@@ -39,25 +52,32 @@ unsafe fn add_callback(handle: Dart_PersistentHandle, key: *const u8) {
 #[no_mangle]
 pub unsafe extern "C" fn register_invoker(invoker: *const CallbackInvoker) {
     unsafe {
-        CB_INVOKER = Some(*invoker);
+        CB_INVOKER = Some(invoker);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn register_callback(handle: Dart_PersistentHandle, key: *const u8) {
+pub unsafe extern "C" fn register_callback(handle: Dart_Handle, key: *const c_char) {
     unsafe {
         add_callback(handle, key);
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn invoke(msg: *const u8, msg_len: usize, key: *const u8) {
+pub unsafe extern "C" fn invoke(msg: *const c_char, msg_len: usize, key: *const c_char) {
     unsafe {
         let handles = HANDLES.get_mut();
         if let Ok(handles) = handles {
-            if let Some(handle) = handles.get(&key) {
-                if let Some(invoker) = CB_INVOKER {
-                    invoker(*handle, msg, msg_len);
+            let c_str: &CStr = CStr::from_ptr(key);
+            if let Ok(id) = c_str.to_str() {
+                if let Some(handle) = handles.get(id) {
+                    if let Some(invoker) = CB_INVOKER {
+                        if let Some(persistent_handle) = Dart_HandleFromPersistent_DL {
+                            let handle = persistent_handle(*handle);
+                            println!("Invoking callback \"{}\" with handle: {:#?}", id, handle);
+                            (*invoker)(handle, msg, msg_len);
+                        }
+                    }
                 }
             }
         }
@@ -69,6 +89,12 @@ pub unsafe extern "C" fn release() {
     unsafe {
         let handles = HANDLES.get_mut();
         if let Ok(handles) = handles {
+            for (id, handle) in &mut *handles {
+                if let Some(free) = Dart_DeletePersistentHandle_DL {
+                    println!("Releasing callback \"{}\" with handle: {:#?}", id, handle);
+                    free(*handle);
+                }
+            }
             handles.clear();
         }
     }
